982605681abdb45e6ef7b8e1fa9aa1994c3a44e0
S_mm <148655586+smm2142@users.noreply.github.com>
2025-05-25 22:17:08 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,7 @@ const cards = document.getElementById("cards");
 const instruction = document.getElementById("instruction");
 
 box.addEventListener("click", () => {
-  // Qutuya animasiya ver
   box.style.transform = "scale(0.9) rotate(10deg)";
-
-  // Yarım saniyə sonra açılan kartları göstər
   setTimeout(() => {
     box.style.display = "none";
     instruction.style.display = "none";
